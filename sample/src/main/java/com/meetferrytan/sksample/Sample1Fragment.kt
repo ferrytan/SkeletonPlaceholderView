61a3e4a26dfad4f1b8b989382400ea2921d868f7
Ferry Irawan <irawan.ferry.tan@gmail.com>
2018-05-23 01:39:54 +0700

    Add custom Bone (Circle & Rect) with custom attributes

--- sample/src/main/java/com/meetferrytan/sksample/Sample1Fragment.kt
+++ sample/src/main/java/com/meetferrytan/sksample/Sample1Fragment.kt
@@ -5,16 +5,26 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.meetferrytan.skeletonplaceholderview.SkeletonPlaceholderView
 import kotlinx.android.synthetic.main.item_placeholder.view.*
+import kotlinx.android.synthetic.main.item_sample_1.view.*
 
 class Sample1Fragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_sample1, container, false)
+        view.title.text = "Title (cornerRadius=2dp)"
+        view.subtitle.text = "Subtitle (cornerRadius=4dp)"
+        view.description.text = "Description (cornerRadius=8dp, customHeight=100)"
+
         view.skeletonPlaceholderView.setView(R.layout.item_sample_1,
-                R.id.img, R.id.title, R.id.subtitle, R.id.description)
+                SkeletonPlaceholderView.RectBone(viewId = R.id.img, cornerRadius = 0f),
+                SkeletonPlaceholderView.RectBone(viewId = R.id.title, cornerRadius = resources.getDimensionPixelSize(R.dimen.radius2dp).toFloat()),
+                SkeletonPlaceholderView.RectBone(viewId = R.id.subtitle, cornerRadius = resources.getDimensionPixelSize(R.dimen.radius4dp).toFloat()),
+                SkeletonPlaceholderView.RectBone(viewId = R.id.description, cornerRadius = resources.getDimensionPixelSize(R.dimen.radius8dp).toFloat(), customHeight = 100))
         view.shimmer.startShimmer()
+
         return view
     }
 
